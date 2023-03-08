fdf11cb766c69d209ced3889e2f93662aaaaffab
ljw13@mail.ustc.edu.cn <ljw13@mail.ustc.edu.cn>
2023-03-08 20:56:01 +0800

    add  to verify my shell program

---
+++ shlab-handout/test.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+touch tsh.out
+make test01 > tsh.out
+for i in 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16
+do
+    make test$i >> tsh.out
+done
+
+sed -i 's|(\b[0-9]*)|(1000)|g;' tsh.out
+sed -i 's|(\b[0-9]*)|(1000)|g;' tshref.out
+
+touch diff.out
+diff tsh.out tshref.out > diff.out
