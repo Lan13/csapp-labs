8b910c8a70c54aebed0babf3522520dc4d03ceff
ljw13@mail.ustc.edu.cn <ljw13@mail.ustc.edu.cn>
2023-04-10 21:03:55 +0800

    finish cache lab part B first part

--- cachelab-handout/trans.c
+++ cachelab-handout/trans.c
@@ -1,4 +1,6 @@
-/* 
+/* Author: Junwei Lan
+ * StudentID: PB20111689
+ * Date: 2023.4.10
  * trans.c - Matrix transpose B = A^T
  *
  * Each transpose function must have a prototype of the form:
@@ -22,6 +24,31 @@ int is_transpose(int M, int N, int A[N][M], int B[M][N]);
 char transpose_submit_desc[] = "Transpose submission";
 void transpose_submit(int M, int N, int A[N][M], int B[M][N])
 {
+    int i, j, k;
+    int v0, v1, v2, v3, v4, v5, v6, v7;
+    for (i = 0; i < N; i += 8) {
+        for (j = 0; j < M; j += 8) {
+            for (k = i; k < i + 8; k++) {
+                v0 = A[k][j];
+                v1 = A[k][j + 1];
+                v2 = A[k][j + 2];
+                v3 = A[k][j + 3];
+                v4 = A[k][j + 4];
+                v5 = A[k][j + 5];
+                v6 = A[k][j + 6];
+                v7 = A[k][j + 7];
+                B[j][k] = v0;
+                B[j + 1][k] = v1;
+                B[j + 2][k] = v2;
+                B[j + 3][k] = v3;
+                B[j + 4][k] = v4;
+                B[j + 5][k] = v5;
+                B[j + 6][k] = v6;
+                B[j + 7][k] = v7;
+            }
+        }
+    }
+    return;
 }
 
 /* 
